6280a13f22711f9d820975e2cb3001b24bb39335
Michal Zmuda <zmu.michal@gmail.com>
2016-01-14 18:58:54 +0100

    less agressive params

--- src/optimisations/simulated_annealing.py
+++ src/optimisations/simulated_annealing.py
@@ -10,8 +10,9 @@ from randomchanges import RandomChanges
 
 
 class SimulatedAnnealing:
-    STARTING_TEMPERATURE = 100
-    MIN_TEMPERATURE = 1e-10
+    STARTING_TEMPERATURE = 1e-2
+    MIN_TEMPERATURE = 1e-15
+    ALPHA = 0.9998
 
     def __init__(self, configuration, assignments):
         self.configuration = configuration
@@ -33,7 +34,8 @@ class SimulatedAnnealing:
 
     def acceptance_probability(self, old_cost, new_cost, T):
         try:
-            ap = math.exp(1000 * (new_cost - old_cost) / T)
+            MULT = 1e1
+            ap = math.exp(MULT * (new_cost - old_cost) / T)
         except OverflowError:
             ap = 1
         return ap
@@ -46,14 +48,13 @@ class SimulatedAnnealing:
 
     def anneal(self):
         temperature = self.STARTING_TEMPERATURE
-        alpha = 0.999
         start = time.time()
         iter_num = 1
         with open('annealing_' + datetime.now().strftime('%Y_%m_%d_%H_%M_%S') + '.csv', 'a') as file:
             file.write("Alg:\t{0}\n".format('annealing'))
             file.write("Starting temperature: {0}\n".format(str(temperature)))
             file.write("Temperature to stop at: {0}\n".format(str(self.MIN_TEMPERATURE)))
-            file.write("Temp. loss factor: {0}\n".format(str(alpha)))
+            file.write("Temp. loss factor: {0}\n".format(str(self.ALPHA)))
             file.write("Iteration;\tElapsed (s);\tQuality (-inf,1]\n")
 
             while temperature > self.MIN_TEMPERATURE:
@@ -72,7 +73,7 @@ class SimulatedAnnealing:
                 if iter_num % 100 == 0:
                     print row
 
-                temperature *= alpha
+                temperature *= self.ALPHA
                 iter_num += 1
 
         return self.current_assignments, self.current_cost, self.best_assignments, self.best_cost
